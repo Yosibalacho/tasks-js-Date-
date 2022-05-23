7c440e55fbba464ae18aad39ce863345af621838
Yosibalacho <yosibala@gmail.com>
2022-05-23 19:54:18 +0300

    date

---
+++ main.js
@@ -0,0 +1,119 @@
+// function dateONTheCreationTime() {
+//    var myDate = new Date();
+//     console.log(myDate);
+// }
+// dateONTheCreationTime()
+
+// function dateAsString(dateString) {
+//     var myDate=new Date(dateString);
+
+//     console.log(myDate);
+// }
+// dateAsString("01/04/1997");
+
+// function dateFromUser(userDate) {
+//    var myDate= new Date(userDate);
+
+//    console.log(myDate);
+
+// };
+// dateFromUser(prompt("Enter Date"));
+// function dateFromUserReturnMonth() {
+//   var userDate = prompt("Enter date");
+//   var myDate = new Date(userDate);
+//   return myDate.getMonth();
+// }
+// dateFromUserReturnMonth();
+// function dateFromUserReturnHour() {
+//   var userDate = prompt("Enter date");
+//   var myDate = new Date(userDate);
+//   return myDate.getHours();
+// }
+// dateFromUserReturnHour();
+// function userDateReturnYear() {
+//     var userDate=prompt("Enter date");
+//     var myDate = new Date(userDate);
+//   return  myDate.getFullYear();
+// }
+// userDateReturnYear();
+// function userDateReturnMinutes() {
+//     var userDate=prompt("Enter date");
+//     var myDate=new Date(userDate);
+//     return myDate.getMinutes()
+// }
+// userDateReturnMinutes();
+// function returnErliestDate() {
+//   var userDate1 = new Date(prompt("Enter first date"));
+//   var userDate2 = new Date(prompt("Enter second date"));
+
+//   if (userDate1 < userDate2) {
+//     return userDate1;
+//   } else {
+//     return userDate2;
+//   }
+// }
+// returnErliestDate();
+
+// function dateReturnAge() {
+//     var userDate= prompt("Enter Birthday date")
+//     return new Date().getFullYear()-new Date(userDate).getFullYear();
+     
+// }
+// dateReturnAge();
+// function userAgeAbove18() {
+//     var myDate=new Date(prompt("Enter birthday date"))
+//     var date18=new Date("01/01/2004")
+//     if(myDate<date18){
+//       alert ("You allowed to enter")
+//     }
+//     else{
+//         alert ("You are not allowed to enter")
+//     }
+// }
+// userAgeAbove18();
+
+function userLoction() {
+    var userDate=new Date(prompt("Enter date"))
+   
+}
+userLoction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function GetRandomDate() {
+//    var rndM= Math.floor(Math.random()*12)
+//    var rndD= Math.floor(Math.random()*30+1)
+//    var rndY= Math.floor(Math.random()*(2023-1990)+1990)
+//    var rndDate= rndM+"/"+rndD+"/"+rndY;
+//    console.log(rndDate);
+   
+// }
+// GetRandomDate();
+
